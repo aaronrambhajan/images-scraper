ddc3523ef302dd5c1957c047375e88d6fe968336
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-01-30 00:19:07 -0500

    Added file containing example queries.

---
+++ example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This will download 6 folders worth of images–300 total.
+# This is to demonstrate how to automate these queries.
+
+# Plastic Bottle
+./get_links.sh Plastic Bottle
+./get_links.sh Plastic Water Bottle
+
+# Plastic Utensil
+./get_links.sh Black Plastic Fork
+./get_links.sh Black Plastic Spoon
+./get_links.sh Plastic Fork
+./get_links.sh Plastic Spoon
